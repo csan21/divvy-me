7c47cc9e9b26d42ecdd81852f9931718086487e8
csan21 <chrisswho@gmail.com>
2017-08-24 14:25:37 -0500

    Add sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def set_session(user)
+    session[:user_id] = user.id
+  end
+
+  def delete_session
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end

e82c7de5a816634e68c9d14ca36c66775bc29e18
csan21 <chrisswho@gmail.com>
2017-09-02 16:33:55 -0500

    Vote helper

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,8 +3,4 @@ class Vote < ApplicationRecord
   belongs_to :restuarant
 
   validates_uniqueness_of :voter_id, scope: :restuarant_id
-
-  def check_vote?(vote, restuarant)
-    vote.voter_id != restuarant.creator_id
-  end
 end

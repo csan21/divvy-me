c43667ae0aefd6daef17786527fdbf6d9c1efccf
csan21 <chrisswho@gmail.com>
2017-09-04 17:00:42 -0500

    Start restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,12 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
     @defaults = Restaurant.default_choices
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+  end
 end

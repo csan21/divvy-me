fac3a2db7d4aff9fec0b5761abf185cdeba60df3
csan21 <chrisswho@gmail.com>
2017-09-07 23:40:54 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :restaurants, only: %i[index create new]
   resources :votes, only: [:create]
-  resource :sessions, only: %i[new create destroy]
+  resources :users, only: %i[new create]
+  resources :sessions, only: %i[new create destroy]
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
